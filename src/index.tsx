2bbecc143b3a061c4aa72eab7af3f512110f3e81
lassallecedric0408 <lassallecedric0408@gmail.com>
2023-09-20 14:08:17 +0200

    Create Header

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import { OmangaProvider } from './context/OmangaContext';
+import { materialUITheme } from './utils/materialUITheme';
+import { ThemeProvider } from '@mui/material/styles';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <OmangaProvider>
-      <App />
-    </OmangaProvider>
+    <ThemeProvider theme={materialUITheme}>
+      <OmangaProvider>
+        <App />
+      </OmangaProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
